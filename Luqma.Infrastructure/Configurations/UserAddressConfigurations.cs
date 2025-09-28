4b719b3ebb3f69842fea2221445e70d162a93988
Mahmoud Darawsheh <darxx03eh@mail.ru>
2025-09-28 15:53:18 +0300

    Create Users Info Tables

---
+++ Luqma.Infrastructure/Configurations/UserAddressConfigurations.cs
@@ -0,0 +1,24 @@
+﻿using Luqma.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Luqma.Infrastructure.Configurations
+{
+    public class UserAddressConfigurations : IEntityTypeConfiguration<UserAddress>
+    {
+        public void Configure(EntityTypeBuilder<UserAddress> builder)
+        {
+            builder.HasKey(address => new
+            {
+                address.UserId,
+                address.City,
+                address.State,
+                address.Street
+            });
+
+            builder.HasOne(address => address.User)
+                .WithMany(user => user.Addresses)
+                .HasForeignKey(address => address.UserId);
+        }
+    }
+}
---
+++ Luqma.Infrastructure/Migrations/20250928125230_UsersInfoTables.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Luqma.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsersInfoTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Deduction",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    FinanceId = table.Column<int>(type: "int", nullable: false),
+                    DeductionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeductionRate = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deduction", x => new { x.UserId, x.FinanceId, x.DeductionDate });
+                    table.ForeignKey(
+                        name: "FK_Deduction_AspNetUsers_FinanceId",
+                        column: x => x.FinanceId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Deduction_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Salary",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    FinanceId = table.Column<int>(type: "int", nullable: false),
+                    SalaryDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Salary", x => new { x.UserId, x.FinanceId, x.SalaryDate });
+                    table.ForeignKey(
+                        name: "FK_Salary_AspNetUsers_FinanceId",
+                        column: x => x.FinanceId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Salary_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserAddress",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAddress", x => new { x.UserId, x.City, x.State, x.Street });
+                    table.ForeignKey(
+                        name: "FK_UserAddress_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deduction_FinanceId",
+                table: "Deduction",
+                column: "FinanceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Salary_FinanceId",
+                table: "Salary",
+                column: "FinanceId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Deduction");
+
+            migrationBuilder.DropTable(
+                name: "Salary");
+
+            migrationBuilder.DropTable(
+                name: "UserAddress");
+        }
+    }
+}
