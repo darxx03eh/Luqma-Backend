5505fb31b0fee889bb81f478fde02c1e2b8ec514
Mahmoud Darawsheh <darxx03eh@mail.ru>
2025-09-29 21:26:29 +0300

    Add Configurations For Menu, MenuContains, MenuItem, Order And OrderItem Tables

--- Luqma.Infrastructure/Configurations/MenuConfigurations.cs
+++ Luqma.Infrastructure/Configurations/MenuConfigurations.cs
@@ -8,12 +8,19 @@ namespace Luqma.Infrastructure.Configurations
     {
         public void Configure(EntityTypeBuilder<Menu> builder)
         {
+            builder.ToTable("Menus");
             builder.HasKey(menu => menu.Id);
 
             builder.HasMany(menu => menu.MenuContains)
                 .WithOne(mc => mc.Menu)
                 .HasForeignKey(mc => mc.MenuId)
                 .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Property(m => m.Title)
+                   .IsRequired()
+                   .HasMaxLength(100);
+            builder.Property(m => m.Description)
+                   .HasMaxLength(500);
         }
     }
 }
