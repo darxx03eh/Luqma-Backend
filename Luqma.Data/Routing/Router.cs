9064cbe28749d01f6d514d350524f7879c01fa36
thamerrrrrr <thamerdraidi@gmail.com>
2025-10-02 12:59:09 +0300

    AddCategory,UpdateCategory,GetCategory,DeleteCategory

--- Luqma.Data/Routing/Router.cs
+++ Luqma.Data/Routing/Router.cs
@@ -36,5 +36,20 @@
             public const string DeactiveUser = $"{prefix}/deactive";
             public const string ActivateUser = $"{prefix}/active";
         }
+        public static class ManagerCategoriesRouting
+        {
+            public const string prefix = $"{rule}Manager/Categories";
+            public const string GetAll = $"{prefix}/GetAll";
+            public const string Add = $"{prefix}/Add";
+            public const string Update = $"{prefix}/Update";
+            public const string Delete = $"{prefix}/Delete{singleRoute}";
+            public const string GetById = $"{prefix}/GetById{singleRoute}";
+        }
+        public static class CustomerCategoriesRouting
+        {
+            public const string prefix = $"{rule}Customer/Categories";
+            public const string GetAll = $"{prefix}/GetAll";
+            public const string GetById = $"{prefix}/GetById{singleRoute}";
+        }
     }
 }
