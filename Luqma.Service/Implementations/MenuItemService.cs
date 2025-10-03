c7dcf9668647edd936f79d22c1b48d97403cab55
thamerrrrrr <thamerdraidi@gmail.com>
2025-10-04 01:36:46 +0300

    some chnges on menu

--- Luqma.Service/Implementations/MenuItemService.cs
+++ Luqma.Service/Implementations/MenuItemService.cs
@@ -21,46 +21,41 @@ namespace Luqma.Service.Implementations
         private readonly IMenuContainsRepository _menuContainsRepository;
 
         public MenuItemService(IMenuItemRepository menuItemRepository 
-            ,ICloudinaryService cloudinaryService,
-            ICategoryItemRepository categoryItemRepository,
-            IMenuContainsRepository menuContainsRepository)
+            ,ICloudinaryService cloudinaryService
+           )
         {
             _menuItemRepository = menuItemRepository;
             _cloudinaryService = cloudinaryService;
-            _categoryItemRepository = categoryItemRepository;
-            _menuContainsRepository = menuContainsRepository;
+          
         }
         public async Task<string> AddMenuItemAsync(MenuItem menuItem,IFormFile file, ICollection<int> CategoryId,ICollection<int>MenuId)
         {
-            var categoryitems = new HashSet<CategoryItem>();
-            var menucontains = new HashSet<MenuContains>();
+           
             if (file != null)
             {
                 var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
                 var imageurl = await _cloudinaryService.UploadFileAsync(file.OpenReadStream(), "Menuitems", fileName);
                 menuItem.ImageUrl = imageurl;
             }
-          var menuitem= await  _menuItemRepository.AddAsync(menuItem);
+         
             foreach(var cat in CategoryId)
             {
-                categoryitems.Add(new CategoryItem()
+                menuItem.CategoryItems.Add(new CategoryItem()
                 {
-                    ItemId = menuitem.Id,
                     CategoryId = cat
                 });
             }
-            menuitem.CategoryItems = categoryitems;
+           
             foreach (var men in MenuId)
             {
-                menucontains.Add(new MenuContains()
+               menuItem.MenuContains.Add(new MenuContains()
                 {
-                    ItemId = menuitem.Id,
+                   
                     MenuId = men
                 });
             }
-            menuItem.MenuContains = menucontains;
-           await  _categoryItemRepository.AddRangeAsync(categoryitems);
-            await _menuContainsRepository.AddRangeAsync(menucontains);
+
+           await  _menuItemRepository.AddAsync(menuItem);
             return "the menuitem is added successfully";
 
             
