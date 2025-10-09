48ef5890bd74cbc6d95959e01fdee50918780518
Mahmoud Darawsheh <darxx03eh@mail.ru>
2025-10-09 12:49:36 +0300

    add code to customer

---
+++ Luqma.Infrastructure/Migrations/20251009094725_AddCodeFieldForCustomerTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Luqma.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCodeFieldForCustomerTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Customers");
+        }
+    }
+}
