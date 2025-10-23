3b9234eae506650dbf5ac40252ec7ae05299cb0c
Mahmoud Darawsheh <darxx03eh@mail.ru>
2025-10-23 18:58:40 +0300

    change configurations

---
+++ Luqma.Infrastructure/Migrations/20251023155714_RestoreCheckConstraints.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Luqma.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RestoreCheckConstraints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_OrderItem_Quantity_Positive",
+                table: "OrderItems");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_OrderItem_Quantity_Positive1",
+                table: "OrderItems",
+                sql: "[Quantity] > 0");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_OrderItem_Quantity_Positive",
+                table: "Carts",
+                sql: "[Quantity] > 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_OrderItem_Quantity_Positive1",
+                table: "OrderItems");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_OrderItem_Quantity_Positive",
+                table: "Carts");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_OrderItem_Quantity_Positive",
+                table: "OrderItems",
+                sql: "[Quantity] > 0");
+        }
+    }
+}
