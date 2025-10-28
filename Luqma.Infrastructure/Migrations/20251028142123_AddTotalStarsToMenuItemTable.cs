73700e803f7685c0820120acde3be88023206882
Mahmoud Darawsheh <darxx03eh@mail.ru>
2025-10-28 16:22:26 +0200

    add total stars for menu item table

---
+++ Luqma.Infrastructure/Migrations/20251028142123_AddTotalStarsToMenuItemTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Luqma.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTotalStarsToMenuItemTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "TotalStars",
+                table: "MenuItems",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_MenuItem_TotalStars_Range",
+                table: "MenuItems",
+                sql: "[TotalStars] >= 0 AND [TotalStars] <= 5");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_MenuItem_TotalStars_Range",
+                table: "MenuItems");
+
+            migrationBuilder.DropColumn(
+                name: "TotalStars",
+                table: "MenuItems");
+        }
+    }
+}
