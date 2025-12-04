1eab9924973eea11d9fb3bba1c9bcad54ba999f2
Mahmoud Darawsheh <darxx03eh@mail.ru>
2025-12-04 16:09:03 +0200

    order tracking method created, add TotalPrice to OrderItems Table

--- Luqma.Service/ModuleServiceServices.cs
+++ Luqma.Service/ModuleServiceServices.cs
@@ -25,7 +25,6 @@ namespace Luqma.Service
             services.AddTransient(typeof(ICategoryItemService), typeof(CategoryItemService));
             services.AddTransient(typeof(IMenuContainService), typeof(MenuContainService));
             services.AddTransient(typeof(IBillService), typeof(BillService));
-
             services.AddTransient(typeof(IKitchenItemsService), typeof(KitchenItemsService));
             services.AddTransient(typeof(IKitchenRequirementsService), typeof(KitchenRequirementsService));
             services.AddTransient(typeof(ICustomerService), typeof(CustomerService));
@@ -35,6 +34,7 @@ namespace Luqma.Service
             services.AddTransient(typeof(IFeedbackService), typeof(FeedbackService));
             services.AddHttpClient<WeatherService>();
             services.AddTransient(typeof(IPaymentService), typeof(PaymentService));
+            services.AddTransient(typeof(IOrderTrackingService), typeof(OrderTrackingService));
             return services;
         }
     }
